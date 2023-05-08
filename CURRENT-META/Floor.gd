7137baee5b431854dc1c245f34317074ec563dbb
marcell-toing <staffaronin@gmail.com>
2023-05-08 16:15:24 -0400

    trying to fill that single tile hole in the floor

---
+++ CURRENT-META/Floor.gd
@@ -0,0 +1,10 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_cell(0, Vector2i(0, 0), 0 , Vector2(0, 0), 0)
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
--- CURRENT-META/Player_0.gd
+++ CURRENT-META/Player_0.gd
@@ -7,8 +7,8 @@ const MOTION_SPEED = 160 # Pixels/second.
 
 func _physics_process(_delta):
 	var motion = Vector2()
-	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	motion.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	motion.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	motion.y /= 2
 	motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
