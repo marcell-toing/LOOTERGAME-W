255ba57cf0d6d4b2b7e9bf71981591bcf0d34956
gah-bo <g.rios1077@gmail.com>
2023-05-09 12:42:26 -0400

    animation and movement

--- CURRENT-META/Player/Player_0.gd
+++ CURRENT-META/Player/Player_0.gd
@@ -4,18 +4,20 @@ extends CharacterBody2D
 #@export makes variables easily editable from the EDITOR
 #TBD seems type checking is 100% needed here? but doc doesnt suggest so? confusion
 @export_category("OUR SPEED VARS")
-@export var MAX_SPEED : int = 160 
-@export var  ACCELERATION : int = 1500 
-@export var  FRICTION : int = 1200 
-
-
-var current_animation = "idle" # TBD implement animations
+@export var MAX_SPEED : int = 500 
+@export var ACCELERATION : int = 3000 
+@export var FRICTION : int = 1200 
 var direction = Vector2.ZERO
-var a = 3
+var last_direction = Vector2.RIGHT
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite.play("idle0")
 
 func _physics_process(delta):
 	move(delta)
-
+	update_animation()
 
 func cartesian_to_isometric(vector):
 	return Vector2(vector.x - vector.y, (vector.x + vector.y) / 2)
@@ -25,18 +27,15 @@ func get_input_direction(): #tbd should we wrap Input with int()?
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return direction.normalized()
 	
-
 func apply_friction(amount):
 	if velocity.length() > amount:
 		velocity -= velocity.normalized() * amount
 	else:
 		velocity = Vector2.ZERO
 
-
 func apply_movement(accel):
 	velocity += accel
 	velocity = velocity.limit_length(MAX_SPEED)
-
 
 func move(delta):
 	direction = get_input_direction()
@@ -46,27 +45,16 @@ func move(delta):
 		apply_friction(motion)
 		
 	else:
-		var motion = direction * ACCELERATION * delta
-		var iso_motion = cartesian_to_isometric(motion)
+		var iso_motion = cartesian_to_isometric(direction * ACCELERATION * delta)
 		apply_movement(iso_motion)
-
+		last_direction = direction
+		
 	move_and_slide()
 
-	func _process(delta):
-		current_animation = "idle"
-		var input_dir = Vector2.ZERO
-		if Input.is_action_pressed("right"):
-			input_dir.x += 1
-		if Input.is_action_pressed("left"):
-			input_dir.x -= 1
-		if Input.is_action_pressed("down"):
-			input_dir.y += 1
-		if Input.is_action_pressed("up"):
-			input_dir.y -= 1
-		input_dir = input_dir.normalized()
-		if input_dir.length() != 0:
-			a = input_dir.angle() / (PI/4)
-			a = wrapi(int(a), 0, 8)
-			current_animation = "run"
-	
-		$AnimatedSprite.animation = current_animation + str(a)
+func update_animation():
+	if direction != Vector2.ZERO:
+		var angle = wrapi(int(direction.angle() / (PI/4)), 0, 8)
+		animated_sprite.play("run" + str(angle))
+	else:
+		var angle = wrapi(int(last_direction.angle() / (PI/4)), 0, 8)
+		animated_sprite.play("idle" + str(angle))
