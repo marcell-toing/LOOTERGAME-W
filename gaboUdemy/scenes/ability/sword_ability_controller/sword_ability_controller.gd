b72600804fc1af0a23cb95067294ad7f319ffdc9
gah-bo <g.rios1077@gmail.com>
2023-05-12 00:30:20 -0400

    blablabla

--- gaboUdemy/scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ gaboUdemy/scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var sword_ability : PackedScene
-@export var CHOOSE_SPAWN_POS : String = "enemy" #player or enemy
+@export var SPAWN_ON_PLAYER : bool = true  #player or enemy
 @export var MAX_RANGE : int = 150
 
 # Called when the node enters the scene tree for the first time.
@@ -9,34 +9,41 @@ func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
 
 
-
 func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
-	var enemies = get_tree().get_nodes_in_group("enemy") 
-	var ability_spawn_pos
+	var closest_enemy_position = get_closest_enemy()
+	if player == null or closest_enemy_position==Vector2.ZERO:
+		return
+			
+	var sword_instance = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance) # DO THIS ASAP? IF DONE AT END, ANIM HAPPENS W/O EFFECT
+	
+	var spawn_pos_offset = + Vector2.RIGHT.rotated(randf_range(0, TAU))*4 	
+	var direction_to_closest_enemy = closest_enemy_position - sword_instance.global_position
+	sword_instance.rotation = direction_to_closest_enemy.angle()
+	
+	if SPAWN_ON_PLAYER:
+		sword_instance.global_position = player.global_position + spawn_pos_offset
+	else:
+		sword_instance.global_position = closest_enemy_position + spawn_pos_offset
+	
+	
+	
+func get_closest_enemy():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-	
-	if CHOOSE_SPAWN_POS=="player":
-		ability_spawn_pos = player
 		
-	if CHOOSE_SPAWN_POS=="enemy":
-		ability_spawn_pos = enemies.filter(func(enemy : Node2D):
-			return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
-		)
-		if ability_spawn_pos.size()==0:
-			return
-		else:
-			ability_spawn_pos.sort_custom(func(a: Node2D, b: Node2D):
+	var enemies = get_tree().get_nodes_in_group("enemy") 
+	var enemies_position = enemies.filter(func(enemy : Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2))
+		
+	if enemies_position.size()==0:
+		return Vector2.ZERO
+	else:
+		enemies_position.sort_custom(func(a: Node2D, b: Node2D):
 				var a_distance = a.global_position.distance_squared_to(player.global_position)
 				var b_distance = b.global_position.distance_squared_to(player.global_position)
 				return a_distance < b_distance
-			)
-	var sword_instance = sword_ability.instantiate() as Node2D
-	player.get_parent().add_child(sword_instance)
-	
-	if CHOOSE_SPAWN_POS=="player":
-		sword_instance.global_position = ability_spawn_pos.global_position
-		
-	if CHOOSE_SPAWN_POS=="enemy":
-		sword_instance.global_position = ability_spawn_pos[0].global_position
+				)
+		return enemies_position[0].global_position
--- gaboUdemy/scenes/basic_enemy/basic_enemy.gd
+++ gaboUdemy/scenes/basic_enemy/basic_enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const MAX_SPEED = 50
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Area2D.area_entered.connect(on_area_entered) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +18,7 @@ func get_direction_to_player():
 	if player_node != null:
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
+
+
+func on_area_entered(other_area: Area2D):
+	queue_free()
